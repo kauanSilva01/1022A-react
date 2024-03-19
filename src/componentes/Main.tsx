0b8111cb4561cf41e91ef1b6a78b238ed4925cb2
kauanSilva01 <116742903+kauanSilva01@users.noreply.github.com>
2024-03-19 10:14:24 -0400

    Box estudante na main.tsx

--- src/componentes/Main.tsx
+++ src/componentes/Main.tsx
@@ -1,10 +1,17 @@
+import './Main.css'
 function Main(){
     return(
-        <div>
-        <main>kauan</main>
-        <h1>meu h1 </h1>
+        <main>
+        <div className="estudante-box">
+            <img className='estudante-foto' src="https://github.com/kauanSilva01.png" alt="" />
+            <div className="informacoes"></div>
+            <h3 className="estudante_nome">Kauan Silvinha</h3>
+            <p className="estudante_descriçao"></p>
+            <a className='link-git' href="#">github</a>
         </div>
+        </main>
 
     )
 }
+
 export default Main;
